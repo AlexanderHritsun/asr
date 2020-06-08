a7394f5b69688bb1045660085005e2aa8eaf833a
Alexander Hritsun <ahritsun@opengov.com>
2020-06-08 13:00:41 +0300

    created

--- src/pages/Services/ServiceCard.jsx
+++ src/pages/Services/ServiceCard.jsx
@@ -1,4 +1,4 @@
-import {Button, Card, Container} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import React from "react";
 import BeautyStars from "beauty-stars";
@@ -11,37 +11,38 @@ export const ServiceCard = ({services}) => {
 
     return (
         <Container className="service-card">
-            {services.map( service => {
-                return (
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://cdn2.vectorstock.com/i/1000x1000/37/86/auto-mechanic-service-flat-icons-of-maintenance-vector-2953786.jpg" />
-                        <Card.Body>
-                            <div>
-                                <Card.Title>{service.name}</Card.Title>
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        marginBottom: 15
-                                    }}
-                                >
-                                    <BeautyStars
-                                        size={15}
-                                        value={service.rating}
-                                    />
-                                </div>
-                            </div>
-                            <Card.Text>
-                                {service.description}
-                            </Card.Text>
-                            <NavLink to={`/services/${service._id}`}>
-                                <Button variant="primary">Просмотреть</Button>
-                            </NavLink>
-                        </Card.Body>
-                    </Card>
 
-                )
-            })}
+                {services.map( service => {
+                    return (
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://cdn2.vectorstock.com/i/1000x1000/37/86/auto-mechanic-service-flat-icons-of-maintenance-vector-2953786.jpg" />
+                                <Card.Body>
+                                    <div>
+                                        <Card.Title>{service.name}</Card.Title>
+                                        <div
+                                            style={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                marginBottom: 15
+                                            }}
+                                        >
+                                            <BeautyStars
+                                                size={15}
+                                                value={service.rating}
+                                            />
+                                        </div>
+                                    </div>
+                                    <NavLink to={`/services/${service._id}`}>
+                                        <Button variant="primary">Просмотреть</Button>
+                                    </NavLink>
+                                    <Card.Text>
+                                        {service.description}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                    )
+                })}
+
         </Container>
     )
 }
